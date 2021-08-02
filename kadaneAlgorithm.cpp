9f7c91f0be5c6ce1de90621392bb6f823c75985a
ATOM_MAX <56764208+ATOMMAX2001@users.noreply.github.com>
2021-08-02 19:45:15 +0530

    Add files via upload

---
+++ kadaneAlgorithm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+void kadanes_algoritham(vector<int>& mem)
+{
+	int max=INT_MIN,total=0;
+	for(int i=0;i<mem.size();i++)
+	{
+		total=total+mem[i];
+		if(max<total){
+			max=total;
+		}
+		
+		if(total<0){
+			total=0;
+		}
+		
+		
+	}
+	cout<<"The Max Sum is "<<max<<endl;
+	
+}
+
+int main()
+{
+	
+    vector<int> a {1,2,3,-2,5};
+    vector<int> b {-1,-2,-3,-4};
+    vector<int> c {-2, -3, 4, -1, -2, 1, 5, -3};
+    
+    kadanes_algoritham(a);
+    kadanes_algoritham(b);
+    kadanes_algoritham(c);
+}
